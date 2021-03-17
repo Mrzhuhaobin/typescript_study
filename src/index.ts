debe3cc09ca2274012cabd0701dbe4cb01e9ad2f
utopa_MrZhu <zhuhaobin@gtland.cn>
2021-03-17 13:44:52 +0800

    feat: ts to test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './polyfill.js';
+
 const identify = <T>(arg: T) : T => {
     return arg
 }
@@ -14,13 +16,22 @@ interface GenericIdentityFn<T> {
 
 let myIdentity: GenericIdentityFn<number> = identify;
 
-const setList = new Set();
+// const setList = new Set();
 const arr :Array<string> = ['a', 'b', 'c']
-arr.forEach(item => {
-    setList.add(item)
-})
-const result :boolean = Object.is(setList, arr);
-// console.log('result', result)
-const keys = Object.keys(arr)
-const values = Object.values(arr)
-console.log('keys', keys, values)
+// arr.forEach(item => {
+//     setList.add(item)
+// })
+// const result :boolean = Object.is(setList, arr);
+// // console.log('result', result)
+// const keys = Object.keys(arr)
+// const values = Object.values(arr)
+// console.log('keys', keys, values)
+
+const obj =  Object.entries(arr)
+
+// console.log('obj', obj)
+enum a {
+    x, y, z
+}
+console.log('a.x', a.x)
+console.log('a', a)
